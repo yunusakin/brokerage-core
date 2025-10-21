d690e9726939858b96175d377ff8cff4ad5e84c6
yunusakin <yunusakinn@gmail.com>
2025-10-21 12:13:56 +0300

    Base Project Structure

---
+++ src/main/java/com/brokerage/core/BrokerageCoreApplication.java
@@ -0,0 +1,13 @@
+package com.brokerage.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokerageCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokerageCoreApplication.class, args);
+	}
+
+}
